7f10ab6add7208d111468c24d9589eafa03b1f1f
kunalvirdi <kunalvirdi.patiala@gmail.com>
2024-03-12 14:52:47 +0530

    added cors

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const generateToken=require('./generateToken')
 const express=require('express')
 require('dotenv').config()
 const app=express()
-
+const cors=require('cors')
 app.use(express.json())
-
+app.use(cors())
 app.post('/generate-token',(req,res)=>{
 	const {uid,channelName}=req.body;
 	const token=generateToken(uid,channelName)
